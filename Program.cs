d4e05fb2995c68a37a010251cbc2050d617c271a
Peter Gallagher <pete@pjgcreations.co.uk>
2019-10-27 11:52:48 +0000

    Add code to flash an LED
    Flashed an LED connected to pin 10 of the Raspberry Pi

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
+using System.Device.Gpio;
+using System.Threading;
 
-namespace dotnetcore3rpi
+namespace rpitest
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            GpioController controller = new GpioController(PinNumberingScheme.Board);
+
+            var pin = 10;
+            var lightTime = 300;
+
+            controller.OpenPin(pin, PinMode.Output);
+
+            try
+            {
+                while(true)
+                {
+                    controller.Write(pin, PinValue.High);
+                    Thread.Sleep(lightTime);
+                    controller.Write(pin, PinValue.Low);
+                    Thread.Sleep(lightTime);
+                }
+            }
+            finally
+            {
+                controller.ClosePin(pin);
+            }
         }
     }
 }
